b9aaa1ee4e79c3a6412bfc37d102ca601ac4563b
SoftandPure <SoftandPure@users.noreply.github.com>
2020-10-22 16:18:13 +0200

    Added basic purge function

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ class PorkchopBot(discord.Client):
 			self.settings = json.load(file)
 		print(self.settings)
 
-		self.run(self.settings['Discord']['Token'])
+		self.run(self.settings['Discord']['token'])
 		print("Bot started...")
 
 	async def on_ready(self):
@@ -20,7 +20,28 @@ class PorkchopBot(discord.Client):
 		if message.author == self.user:
 			return
 
-		await message.channel.send("This is a test message")
+		if message.content[0] == '!':
+			if 'purge' in message.content:
+				await self.purge(message, 10)
+		elif message.content[0] == '?':
+			await message.channel.send("This is a question")
+
+	async def purge (self, message, range):
+		  """
+		  This function purges messages from the message channel.
+		  :param message: The recieved command.
+		  :param range: The amount of messages to delete.
+
+		  TODO: Implement the range functionality.
+		  """
+
+	  	range = int(range)
+	  	message_counter = 0
+
+	  	async for msg in message.channel.history(limit=range):
+	  		await msg.delete()
+	  		message_counter +=1
+	  	print(f'deleted {message_counter} messages from {message.channel}!')
 
 
 if __name__ == '__main__':
